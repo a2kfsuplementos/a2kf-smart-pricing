93b5cf092f90e56db2951c044357e6c4a3aea076
A2KF Suplementos <a2kfsupps@gmail.com>
2026-06-29 19:14:23 -0300

    Add normalizeProduct function for product normalization

--- backend/services/importer/normalizeProduct.js
+++ backend/services/importer/normalizeProduct.js
@@ -1,1 +1,23 @@
+function normalizeProduct(product) {
 
+    return {
+
+        sku: product.sku,
+
+        name: product.name,
+
+        brand: product.brand,
+
+        category: product.category,
+
+        price: Number(product.price),
+
+        image: product.image,
+
+        url: product.url
+
+    };
+
+}
+
+module.exports = normalizeProduct;
