2919a4bdb532e9b3aaee347f7d60111e7859262e
A2KF Suplementos <a2kfsupps@gmail.com>
2026-06-29 19:13:29 -0300

    Implement XML download function using axios

--- backend/services/importer/downloadXml.js
+++ backend/services/importer/downloadXml.js
@@ -1,1 +1,11 @@
+const axios = require("axios");
 
+async function downloadXml() {
+
+    const response = await axios.get(process.env.YAMPI_XML);
+
+    return response.data;
+
+}
+
+module.exports = downloadXml;
