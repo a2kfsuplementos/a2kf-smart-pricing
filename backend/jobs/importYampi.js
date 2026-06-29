d41ea4d24b4171c3db84c57fad5ac41f48c1045f
A2KF Suplementos <a2kfsupps@gmail.com>
2026-06-29 19:14:51 -0300

    Update importYampi.js

--- backend/jobs/importYampi.js
+++ backend/jobs/importYampi.js
@@ -1,1 +1,18 @@
+const downloadXml = require("../services/importer/downloadXml");
+const parseXml = require("../services/importer/parseXml");
 
+async function executeImport() {
+
+    console.log("Iniciando importação...");
+
+    const xml = await downloadXml();
+
+    const data = parseXml(xml);
+
+    console.log("XML carregado.");
+
+    console.log(data);
+
+}
+
+executeImport();
