48aabe8a8708026255b354499c24e03018a78190
A2KF Suplementos <a2kfsupps@gmail.com>
2026-06-29 18:21:09 -0300

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,15 @@
 const express = require("express");
 
+const healthRoute = require("./routes/health");
+
 const app = express();
 
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("🚀 A2KF Smart Pricing API");
+  res.send("🚀 A2KF Smart Pricing API");
 });
+
+app.use("/api/health", healthRoute);
 
 module.exports = app;
