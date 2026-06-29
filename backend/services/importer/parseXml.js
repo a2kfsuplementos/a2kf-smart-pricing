3ca4740b525aaa1c93768d71e4b42b08c5bce953
A2KF Suplementos <a2kfsupps@gmail.com>
2026-06-29 19:13:51 -0300

    Add XML parsing functionality using fast-xml-parser

--- backend/services/importer/parseXml.js
+++ backend/services/importer/parseXml.js
@@ -1,1 +1,13 @@
+const { XMLParser } = require("fast-xml-parser");
 
+const parser = new XMLParser({
+    ignoreAttributes: false
+});
+
+function parseXml(xml) {
+
+    return parser.parse(xml);
+
+}
+
+module.exports = parseXml;
